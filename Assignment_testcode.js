0be38009d3e0d6757b0ff4be131f3c325c2e3008
nair-vishal <vshal17@gmail.com>
2026-03-01 13:16:35 +0530

    Add files via upload

---
+++ Assignment_testcode.js
@@ -0,0 +1,13 @@
+var firstName = "Vishal";
+var companyName = "JPMorgan";
+var mobilenumber = "1234567890";
+var isAutomation = true;
+var hasPlaywright;
+
+
+//Print the values of variables
+console.log("First Name: " + firstName);
+console.log("Company Name: " + companyName);
+console.log("Mobile Number; " + mobilenumber);
+console.log("is Automation: " + isAutomation);
+console.log("Has Playwright: " + hasPlaywright);
